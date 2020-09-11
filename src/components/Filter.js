0ef6a14c0e072c59c535eef63de001fd2d4c60f3
sz16900 <sz16900@my.bristol.ac.uk>
2020-09-11 09:59:36 -1000

    the header + the filer

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -6,23 +6,25 @@ import PropTypes from 'prop-types';
 function Home({ fetchPairings }) {
   return (
     <div className="flex flex-col border-black border-b-2 p-4">
-      <span className="text-4xl self-center">Beer + ?</span>
-      <form className="self-center">
-        <input
-          class="appearance-none border-b rounded w-full text-center text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username"
-          type="text"
-          placeholder="Pizza"
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              e.preventDefault();
-              fetchPairings(e.target.value);
-            } else {
-              fetchPairings(e.target.value);
-            }
-          }}
-        ></input>
-      </form>
+      <div className="flex self-center">
+        <span className="text-4xl ">Beer + </span>
+        <form className="self-center">
+          <input
+            class="appearance-none border-b rounded w-full text-center text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Pizza"
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+                fetchPairings(e.target.value);
+              } else {
+                fetchPairings(e.target.value);
+              }
+            }}
+          ></input>
+        </form>
+      </div>
     </div>
   );
 }
